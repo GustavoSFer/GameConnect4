dc35619c188a7534fe6876fb066a6af8f23fa42c
GustavoSFer <gustavo.sfernander@gmail.com>
2025-01-12 15:23:47 -0300

    Feat: Iniciando o projeto

---
+++ src/main/java/br/com/fernandes/Connect4GameApplication.java
@@ -0,0 +1,13 @@
+package br.com.fernandes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Connect4GameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Connect4GameApplication.class, args);
+	}
+
+}
